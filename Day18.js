428ab3466d247c8719ef5f9f86004a14cfacf8ca
Fikri Anuar <70595834+FikriAnuar@users.noreply.github.com>
2021-12-18 10:27:49 -0800

    Create Day18.js
    This problem took me a while, but I finally got it to work. Definitely not the most efficient and very error-prone... I spent a lot of time debugging.
    Part 2 was fairly simple although I had to change my calcMagnitude function to return a parseInt version of the result so that I could properly compare number values.

---
+++ Day18.js
@@ -0,0 +1,316 @@
+var input = `[[[9,2],[[2,9],0]],[1,[[2,3],0]]]
+[[[[2,0],2],[[6,4],[7,3]]],[0,[[3,0],[0,6]]]]
+[[[[7,2],2],[9,[6,5]]],[[2,4],5]]
+[[[[7,8],2],1],[[[5,4],[2,9]],[7,8]]]
+[[[0,7],[1,[6,6]]],[[[0,7],9],4]]
+[[[3,[9,6]],[5,1]],[[[0,1],6],[[7,6],0]]]
+[[[3,0],[7,[4,0]]],[4,[[6,6],[5,3]]]]
+[[[1,[4,8]],[2,[5,8]]],[[[3,6],[2,2]],[[3,8],[7,0]]]]
+[9,[[[5,0],[0,3]],[2,[2,6]]]]
+[[[3,[8,2]],[[8,0],5]],[[[7,6],[4,9]],[7,5]]]
+[[7,[[4,1],9]],[5,1]]
+[[[5,[7,5]],1],[8,[5,8]]]
+[[[[0,2],7],[[1,4],[9,8]]],[[3,[0,3]],7]]
+[[[[4,3],[7,4]],[6,[6,4]]],[8,0]]
+[[[1,1],1],[[5,[2,7]],7]]
+[[[5,4],5],[[7,[6,3]],[[8,4],6]]]
+[[[7,9],[[4,4],[0,0]]],[[[8,6],6],[2,[6,4]]]]
+[[[[4,7],[4,9]],3],[[[7,1],[8,6]],[9,[8,2]]]]
+[6,[6,[2,9]]]
+[[4,[[5,5],[5,0]]],[[[3,4],[9,5]],[8,6]]]
+[2,[0,[2,5]]]
+[[[4,[7,1]],[2,8]],[[7,0],[[1,6],1]]]
+[[[3,4],[[7,8],[6,7]]],[[[6,2],[1,2]],5]]
+[[[8,[0,8]],[[9,9],0]],[[[3,5],[4,2]],7]]
+[[0,[[0,3],2]],[4,1]]
+[[[[0,4],6],7],[[4,[9,1]],3]]
+[[0,[[7,0],8]],[2,[8,[8,2]]]]
+[[[[3,6],2],[9,4]],[6,[[7,9],[4,5]]]]
+[[[[4,9],1],[[9,6],[8,8]]],[[7,[7,6]],[[8,3],[9,0]]]]
+[2,0]
+[[[[8,2],0],[3,5]],[[7,2],0]]
+[[[[1,9],9],6],[9,[[9,3],[8,7]]]]
+[[[9,[4,0]],[[7,1],[4,4]]],[[4,[2,3]],[8,7]]]
+[[[[9,7],[5,6]],[4,[6,7]]],7]
+[5,[[[8,2],8],[6,[7,9]]]]
+[0,[[9,[0,1]],[[8,3],7]]]
+[[[[4,5],[4,2]],[[5,2],[3,1]]],[[[3,1],[8,5]],8]]
+[[0,4],[[2,[2,6]],[[1,1],3]]]
+[[[0,8],[7,[5,8]]],7]
+[[[7,2],[[6,6],[2,7]]],[[0,[9,3]],2]]
+[[[[0,9],2],[[6,0],4]],3]
+[[5,[[9,6],9]],[[6,[1,2]],[1,[6,2]]]]
+[[[[3,9],5],[9,[7,2]]],[5,[[3,4],[0,6]]]]
+[[2,[6,7]],[0,[[2,0],7]]]
+[[2,[[5,4],[2,1]]],[2,[[8,7],[5,3]]]]
+[[[[0,4],[2,5]],[1,2]],[5,[8,[0,3]]]]
+[[[[9,2],[3,2]],[[2,9],4]],5]
+[[[[8,9],5],1],[9,3]]
+[[5,2],[3,[[8,5],2]]]
+[[[0,1],[7,8]],[[[6,2],4],[[6,2],[9,5]]]]
+[[[[9,6],5],2],2]
+[[[[3,2],3],3],[[[0,1],1],[[8,4],8]]]
+[[4,[2,[3,0]]],[[6,[7,0]],6]]
+[[6,[[7,8],3]],[[[2,7],4],9]]
+[0,2]
+[[[9,1],[[3,7],[6,0]]],[[0,[4,1]],[[5,4],7]]]
+[[[3,[9,4]],8],[[5,3],2]]
+[[6,6],[[[0,5],[0,9]],[[5,5],4]]]
+[[[[1,2],4],[[2,4],[8,0]]],[0,[[4,4],[5,8]]]]
+[0,[[[9,0],3],[8,4]]]
+[[4,5],[[[9,9],[3,5]],[8,[1,4]]]]
+[[7,8],[[[3,1],[7,0]],[[4,7],[9,1]]]]
+[[4,[2,[1,9]]],[[6,[6,1]],[[0,3],3]]]
+[[[5,[0,9]],6],[[[3,4],[9,6]],[[4,0],[0,4]]]]
+[[[1,5],[8,[2,8]]],[[5,[0,8]],[[0,7],[4,6]]]]
+[[9,[0,2]],[[3,3],[3,1]]]
+[[[[2,8],[5,9]],[2,[1,5]]],9]
+[[3,[[8,9],[3,1]]],[[[9,0],7],[[0,4],3]]]
+[[[[1,5],2],[5,[5,9]]],[5,[[0,1],[0,2]]]]
+[6,[[[0,4],8],[[8,2],[5,5]]]]
+[[[[7,7],5],[[8,2],7]],[2,5]]
+[[[1,1],[[7,8],0]],3]
+[[6,[[4,2],9]],[[[5,4],4],3]]
+[[[[5,8],3],[[0,4],9]],[[[2,9],2],[3,4]]]
+[[0,[4,8]],6]
+[[[[9,5],[1,9]],[[3,7],[5,5]]],8]
+[[1,9],6]
+[[[4,[1,5]],3],0]
+[[[2,[6,9]],5],[[5,7],[5,[7,1]]]]
+[[[[3,1],[7,3]],[[1,0],[4,6]]],[[[4,9],[4,1]],[9,[2,0]]]]
+[[[5,0],[[9,4],6]],[1,[[0,4],[9,9]]]]
+[[[[9,8],3],[7,5]],[[[9,5],2],[9,9]]]
+[[8,[[8,0],[2,3]]],[[[3,8],[2,6]],[[1,0],0]]]
+[[[7,[7,1]],[[6,6],[2,9]]],[[5,[2,0]],[[3,9],[7,4]]]]
+[1,[4,[[9,7],[1,3]]]]
+[[0,3],[[[4,1],7],[[4,1],[3,0]]]]
+[[0,[[7,7],6]],[[4,9],2]]
+[[0,8],[4,[4,5]]]
+[[[8,[0,5]],[[1,3],[0,5]]],[[2,6],[1,5]]]
+[[[[7,6],8],[0,[2,7]]],8]
+[8,[[[5,4],8],[[2,1],[7,5]]]]
+[[[[7,3],[7,1]],0],[[[7,9],2],3]]
+[[8,5],[6,6]]
+[[[[5,2],8],7],[[[6,8],[1,0]],[[0,0],1]]]
+[[[[1,0],1],6],[9,8]]
+[[[[1,2],7],[1,[2,8]]],[[8,1],[[7,5],2]]]
+[[0,6],[[2,8],[9,0]]]
+[[[0,[7,7]],[2,[0,8]]],[[[7,4],4],[7,[4,0]]]]
+[[[2,[9,3]],[[3,7],3]],[[[9,7],[5,6]],8]]
+[[2,[[8,7],2]],[[8,[1,8]],[[7,2],1]]]`;
+
+var fishes = input.split(`\n`);
+
+/*
+var addTwoFish = (fishA, fishB) => {
+  var result = '[' + fishA + ',' + fishB + ']';
+  return result;
+}*/
+
+var reduceFish = (fish) => {
+  var isReduced = false;
+  var exploded = false;
+  var splitted = false;
+  var times = 0;
+  while (!isReduced) {
+    [fish, exploded] = checkNested(fish);
+    if (!exploded) {
+      [fish, splitted] = checkSplit(fish);
+    }
+    if (!exploded && !splitted) {
+      isReduced = true;
+    }
+    times++;
+  }
+  return fish;
+}
+
+var checkNested = (fish) => {
+  var currentLevel = 0;
+  var lastLeftIndex = -1;
+  var lastComma = -1;
+  for (var i = 0; i < fish.length; i++) {
+    if (fish[i] === '[') {
+      currentLevel++;
+    } else if (fish[i] === ']') {
+      currentLevel--;
+    } else if (fish[i] === ',') {
+      lastComma = i;
+    } else {
+      if (currentLevel > 4) {
+        var rightExplodeNumFound = false;
+        var lastLeftExplodeNumIndex = -1;
+        var lastRightExplodeIndex = -1;
+        var firstRightExplodeIndex = -1;
+        var nextRightBracket = fish.length-1;
+        var firstLeftExplodeIndex = i;
+        var firstRightIndex = -1;
+        var lastRightIndex = -1;
+        var commaFound = false;
+        var nextNumber = false;
+        var nextNumberFound = false;
+        i++;
+        while (i < fish.length && lastRightIndex === -1) {
+          if (!rightExplodeNumFound) {
+            if (fish[i] !== '[' && fish[i] !== ']' && fish[i] !== ',' && commaFound) {
+              rightExplodeNumFound = true;
+              firstRightExplodeIndex = i;
+            }
+            if (fish[i] === ',') {
+              commaFound = true;
+              if (lastLeftExplodeNumIndex === -1) {
+                lastLeftExplodeNumIndex = i;
+              }
+            }
+          } else {
+            if (nextNumber) {
+              if (!nextNumberFound) {
+                if (fish[i] !== '[' && fish[i] !== ']' && fish[i] !== ',') {
+                  nextNumberFound = true;
+                  firstRightIndex = i;
+                }
+              } else {
+                if (fish[i] === '[' || fish[i] === ']' || fish[i] === ',') {
+                  if (lastRightIndex === -1) {
+                    lastRightIndex = i;
+                  }
+                }
+              }
+            } else {
+              if (fish[i] === '[' || fish[i] === ']' || fish[i] === ',') {
+                lastRightExplodeIndex = i-1;
+                nextNumber = true;
+              }
+            }
+          }
+          i++;
+        }
+        var leftExplodeNum = fish.slice(firstLeftExplodeIndex, lastLeftExplodeNumIndex);
+        //debugger;
+        var leftString = fish.slice(0,firstLeftExplodeIndex-1);
+        if (lastLeftIndex !== -1) {
+          var firstLeftIndex = lastLeftIndex;
+          while (fish[firstLeftIndex-1] !== '[' && fish[firstLeftIndex-1] !== ']' && fish[firstLeftIndex-1] !== ',') {
+            firstLeftIndex--;            
+          }
+          var leftNum = fish.slice(firstLeftIndex, lastLeftIndex+1);
+          leftString = leftString.split('');
+          var newNumber = parseInt(leftNum) + parseInt(leftExplodeNum);
+          leftString.splice(firstLeftIndex, (lastLeftIndex+1-firstLeftIndex), newNumber);
+          leftString = leftString.join('');
+        }
+        var rightString = fish.slice(lastRightExplodeIndex+2);
+        if (firstRightIndex !== -1) {
+          var rightExplodeNum = fish.slice(firstRightExplodeIndex, lastRightExplodeIndex+1);
+          var rightNum = fish.slice(firstRightIndex, lastRightIndex);
+          rightString = rightString.split('');
+          var newNumber = parseInt(rightNum) + parseInt(rightExplodeNum);
+          rightString.splice(firstRightIndex-(lastRightExplodeIndex+2), (lastRightIndex-firstRightIndex), newNumber);
+          rightString = rightString.join('');
+        }
+        fish = leftString + '0' + rightString;
+        return [fish, true];
+      }
+      lastLeftIndex = i;
+    }
+  }
+  return [fish, false];
+}
+
+var checkSplit = (fish) => {
+  var numStartIndex = 0;
+  for (var i = 0; i < fish.length; i++) {
+    if (fish[i] === '[' || fish[i] === ']' || fish[i] === ',') {
+      numStartIndex = i + 1;
+    } else {
+      if (numStartIndex !== i) {
+        var numEndIndex = i;
+        while (fish[i] !== '[' && fish[i] !== ']' && fish[i] !== ',') {
+          numEndIndex = i + 1;
+          i++;
+        }
+        var splitNumber = parseInt(fish.slice(numStartIndex, numEndIndex));
+        var leftNum = Math.floor(splitNumber/2);
+        var rightNum = Math.ceil(splitNumber/2);
+        var newValues = '[' + leftNum + ',' + rightNum + ']';
+        fish = fish.split('');
+        fish.splice(numStartIndex, numEndIndex-numStartIndex, newValues);
+        fish = fish.join('');
+        return [fish, true];
+      }
+    }
+  }
+  return [fish, false];
+}
+
+var calcMagnitude = (fish) => {
+  while (fish[0] === '[') {
+  var seenLeftBracket = false;
+  var seenComma = false;
+  var leftBracketIndex = 0;
+  for (var i = 0; i < fish.length; i++) {
+    if (seenLeftBracket) {
+      if (seenComma) {
+        if (fish[i] === ']') {
+          var pair = fish.slice(leftBracketIndex, i+1);
+          fish = fish.split('');
+          fish.splice(leftBracketIndex, i+1-leftBracketIndex, findMagnitude(pair));
+          fish = fish.join('');
+          seenComma = false;
+          seenLeftBracket = false;
+        } else {
+          if (fish[i] === '[') {
+            seenLeftBracket = true;
+            leftBracketIndex = i;
+            seenComma = false
+          }
+        }
+      } else if (fish[i] === ',') {
+        seenComma = true;
+      }
+    }
+    if (fish[i] === '[') {
+      seenLeftBracket = true;
+      leftBracketIndex = i;
+    }
+  }
+  }
+  return parseInt(fish);
+}
+
+var findMagnitude = (pair) => {
+  var commaIndex = 0;
+  for (var i = 0; i < pair.length; i++) {
+    if (pair[i] === ',') {
+      commaIndex = i;
+    }
+  }
+  var leftNum = parseInt(pair.slice(1, commaIndex));
+  var rightNum = parseInt(pair.slice(commaIndex+1, pair.length-1));
+  return 3*leftNum + 2*rightNum;
+}
+
+var currentFish = fishes[0];
+
+
+for (var i = 1; i < fishes.length; i++) {
+  currentFish = '[' + currentFish + ',' + fishes[i] + ']';
+  currentFish = reduceFish(currentFish);
+}
+
+console.log(`Part 1:`, calcMagnitude(currentFish));
+
+var largestMagnitude = 0;
+for (var i = 0; i < fishes.length; i++) {
+  for (var j = 0; j < fishes.length; j++) {
+    if (i !== j) {
+      var fishSum = '[' + fishes[i] + ',' + fishes[j] + ']';
+      var reducedFish = reduceFish(fishSum);
+      var magnitude = calcMagnitude(reducedFish);
+      if (magnitude > largestMagnitude) {
+        largestMagnitude = magnitude;
+      }
+    }
+  }
+}
+console.log(`Part 2: `, largestMagnitude);
